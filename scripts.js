c47a6a79500cc963eb020e178b8af1d4740583f8
AlxUnd3r777 <114871869+AlxUnd3r777@users.noreply.github.com>
2024-09-02 19:48:55 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Seleccionar elementos del DOM
     const openScannerBtn = document.getElementById('openScannerBtn');
-    const openPageBtn = document.getElementById('openPageBtn');
     const scannerModal = document.getElementById('scannerModal');
     const closeModal = document.querySelector('.close');
 
+    console.log("Script cargado correctamente");
+
+    // Verifica si el botón existe
+    if (openScannerBtn) {
+        console.log("Botón de abrir escáner encontrado");
+    } else {
+        console.log("Botón de abrir escáner no encontrado");
+    }
+
     // Abrir el modal del escáner de QR
     openScannerBtn.addEventListener('click', function() {
+        console.log("Botón de abrir escáner clickeado");
         scannerModal.style.display = 'block';
     });
 
     // Cerrar el modal del escáner de QR
     closeModal.addEventListener('click', function() {
+        console.log("Botón de cerrar clickeado");
         scannerModal.style.display = 'none';
     });
 
     // Cerrar el modal si se hace clic fuera del contenido del modal
     window.addEventListener('click', function(event) {
         if (event.target == scannerModal) {
+            console.log("Clic fuera del modal detectado");
             scannerModal.style.display = 'none';
         }
     });
