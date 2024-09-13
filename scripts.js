5cb14dee2932da70f75b9a5b111c3e7e89232409
AlxUnd3r777 <114871869+AlxUnd3r777@users.noreply.github.com>
2024-09-13 14:39:17 -0300

    Actualizar scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // Mostrar/Ocultar el botón según la posición del scroll
 window.onscroll = function() {
     const btn = document.getElementById("back-to-top-btn");
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    if (document.documentElement.scrollTop > 200) {
         btn.style.display = "block";
     } else {
         btn.style.display = "none";
@@ -19,26 +19,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const scannerModal = document.getElementById('scannerModal');
     const closeModal = document.querySelector('.close');
 
-    console.log("Script cargado correctamente");
-
     // Verifica si el botón existe
     if (openScannerBtn) {
-        console.log("Botón de abrir escáner encontrado");
-        
         // Abrir el modal del escáner de QR
         openScannerBtn.addEventListener('click', function() {
-            console.log("Botón de abrir escáner clickeado");
             scannerModal.style.display = 'block';
         });
-    } else {
-        console.log("Botón de abrir escáner no encontrado");
     }
 
     // Verifica si el botón de cerrar el modal existe
     if (closeModal) {
         // Cerrar el modal del escáner de QR
         closeModal.addEventListener('click', function() {
-            console.log("Botón de cerrar clickeado");
             scannerModal.style.display = 'none';
         });
     }
@@ -46,7 +38,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Cerrar el modal si se hace clic fuera del contenido del modal
     window.addEventListener('click', function(event) {
         if (event.target === scannerModal) {
-            console.log("Clic fuera del modal detectado");
             scannerModal.style.display = 'none';
         }
     });
