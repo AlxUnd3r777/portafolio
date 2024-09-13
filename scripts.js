41a5241f3e88f12819e98f6340a1c2f65638733e
AlxUnd3r777 <114871869+AlxUnd3r777@users.noreply.github.com>
2024-09-13 14:36:43 -0300

    Actualizar scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
+// Mostrar/Ocultar el botón según la posición del scroll
+window.onscroll = function() {
+    const btn = document.getElementById("back-to-top-btn");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+// Función para subir al top cuando se hace clic en el botón
+document.getElementById("back-to-top-btn").addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     // Seleccionar elementos del DOM
     const openScannerBtn = document.getElementById('openScannerBtn');
