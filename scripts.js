e189b4a9b3ca4d462f3cbd1595b09d4b9718aa90
AlxUnd3r777 <114871869+AlxUnd3r777@users.noreply.github.com>
2024-09-13 14:37:54 -0300

    Actualizar scripts.js

--- scripts.js
+++ scripts.js
@@ -13,11 +13,6 @@ document.getElementById("back-to-top-btn").addEventListener("click", function() 
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
-
-
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     // Seleccionar elementos del DOM
     const openScannerBtn = document.getElementById('openScannerBtn');
@@ -29,25 +24,28 @@ document.addEventListener('DOMContentLoaded', function() {
     // Verifica si el botón existe
     if (openScannerBtn) {
         console.log("Botón de abrir escáner encontrado");
+        
+        // Abrir el modal del escáner de QR
+        openScannerBtn.addEventListener('click', function() {
+            console.log("Botón de abrir escáner clickeado");
+            scannerModal.style.display = 'block';
+        });
     } else {
         console.log("Botón de abrir escáner no encontrado");
     }
 
-    // Abrir el modal del escáner de QR....
-    openScannerBtn.addEventListener('click', function() {
-        console.log("Botón de abrir escáner clickeado");
-        scannerModal.style.display = 'block';
-    });
-
-    // Cerrar el modal del escáner de QR
-    closeModal.addEventListener('click', function() {
-        console.log("Botón de cerrar clickeado");
-        scannerModal.style.display = 'none';
-    });
+    // Verifica si el botón de cerrar el modal existe
+    if (closeModal) {
+        // Cerrar el modal del escáner de QR
+        closeModal.addEventListener('click', function() {
+            console.log("Botón de cerrar clickeado");
+            scannerModal.style.display = 'none';
+        });
+    }
 
     // Cerrar el modal si se hace clic fuera del contenido del modal
     window.addEventListener('click', function(event) {
-        if (event.target == scannerModal) {
+        if (event.target === scannerModal) {
             console.log("Clic fuera del modal detectado");
             scannerModal.style.display = 'none';
         }
